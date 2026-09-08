320c8a0b58f727ed5098c07fe3cc3378b885511b
Photo Wallpaper Dev <photowallpaper@app.dev>
2026-09-08 18:03:04 +0700

    Fix missing import: ConnectivityManager in MainActivity

--- app/src/main/java/com/photowallpaper/MainActivity.kt
+++ app/src/main/java/com/photowallpaper/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.photowallpaper
 
 import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
